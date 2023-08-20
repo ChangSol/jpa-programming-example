5e4c61a3ee83ac282137c0bbd1cf8d55ce2ffbda
ChangSol <ckdwnsla12@naver.com>
2023-08-20 18:14:21 +0900

    Entity 양방향 연관관계 설정

--- modules/app/src/main/java/org/changsol/apps/members/domains/Member.java
+++ modules/app/src/main/java/org/changsol/apps/members/domains/Member.java
@@ -1,10 +1,16 @@
 package org.changsol.apps.members.domains;
 
+import com.google.common.collect.Sets;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.validation.constraints.NotNull;
+import org.changsol.apps.orders.domains.Order;
 
 @Entity
 public class Member {
@@ -20,4 +26,8 @@ public class Member {
 	private String street;
 
 	private String zipcode;
+
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OrderBy("id asc ")
+	private Set<Order> orders = Sets.newHashSet();
 }
--- modules/app/src/main/java/org/changsol/apps/products/domains/Product.java
+++ modules/app/src/main/java/org/changsol/apps/products/domains/Product.java
@@ -1,10 +1,17 @@
 package org.changsol.apps.products.domains;
 
+import com.google.common.collect.Sets;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.validation.constraints.NotNull;
+import org.changsol.apps.orders.domains.Order;
+import org.changsol.apps.orders.domains.OrderItem;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
@@ -23,4 +30,8 @@ public class Product {
 	@NotNull
 	@ColumnDefault("0")
 	private Integer stockQty = 0;
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OrderBy("id asc ")
+	private Set<OrderItem> orderItems = Sets.newHashSet();
 }
