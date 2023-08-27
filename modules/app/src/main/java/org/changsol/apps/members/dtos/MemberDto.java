dd8cf7d73c2f6a7bac0e812e1590cba5da3623a5
ChangSol <ckdwnsla12@naver.com>
2023-08-27 20:48:20 +0900

    Member Controller, Dto Swagger Annotation Add

--- modules/app/src/main/java/org/changsol/apps/members/dtos/MemberDto.java
+++ modules/app/src/main/java/org/changsol/apps/members/dtos/MemberDto.java
@@ -1,5 +1,6 @@
 package org.changsol.apps.members.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,15 +9,20 @@ public class MemberDto {
 
 	@Getter
 	@Setter
+	@Schema(title = "Member Create")
 	public static class Create {
 
+		@Schema(description = "이름", requiredMode = Schema.RequiredMode.REQUIRED)
 		@NotBlank(message = "name is required")
 		private String name;
 
+		@Schema(description = "도시", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 		private String city;
 
+		@Schema(description = "주소", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 		private String street;
 
+		@Schema(description = "우편번호", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 		private String zipcode;
 	}
 }
